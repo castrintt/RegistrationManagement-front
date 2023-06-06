744e98ee9ed3ccc9f0302b525594f76eee6cf05f
Castro <igordc38@gmail.com>
2023-06-06 11:59:45 -0300

    ICA [feature] unauthorize screen + not found (both responsive)

--- src/App/Pages/Common/NotFound/NotFound.tsx
+++ src/App/Pages/Common/NotFound/NotFound.tsx
@@ -1,10 +1,35 @@
-import React from 'react'
-
+import React from "react";
+import styles from "./NotFound.module.css";
+import { MdDangerous } from "react-icons/md";
+import Buttons from "../../../Components/Buttons/Buttons";
+import { useNavigate } from "react-router-dom";
 
 const NotFound = () => {
-  return (
-    <div>NotFound</div>
-  )
-}
+  const navigate = useNavigate();
 
-export default NotFound
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <MdDangerous />
+        <h1>Página não encontrada</h1>
+        <span>Por favor, escolha uma opção:</span>
+        <div className={styles.buttons_container}>
+          <Buttons
+            onClickMethod={() => navigate(-1)}
+            buttonText={"voltar a ultima tela"}
+            variant={"edit"}
+            width={"100%"}
+          />
+          <Buttons
+            onClickMethod={() => navigate("/login")}
+            buttonText={"ir ao login"}
+            variant={"create"}
+            width={"100%"}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
--- src/App/Pages/Common/Unauthorize/Unauthorize.tsx
+++ src/App/Pages/Common/Unauthorize/Unauthorize.tsx
@@ -1,7 +1,35 @@
 import React from "react";
+import styles from "./Unauthorize.module.css";
+import { IoIosAlert } from "react-icons/io";
+import Buttons from "../../../Components/Buttons/Buttons";
+import { useNavigate } from "react-router-dom";
 
 const Unauthorize = () => {
-  return <div>Unauthorize</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <IoIosAlert />
+        <h1>Sem acesso!</h1>
+        <span>Por favor, escolha uma opção:</span>
+        <div className={styles.buttons_container}>
+          <Buttons
+            onClickMethod={() => navigate(-1)}
+            buttonText={"voltar a ultima tela"}
+            variant={"edit"}
+            width={"100%"}
+          />
+          <Buttons
+            onClickMethod={() => navigate("/login")}
+            buttonText={"ir ao login"}
+            variant={"create"}
+            width={"100%"}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Unauthorize;
