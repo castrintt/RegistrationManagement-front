ca552b7e83cefdaaa36f5b017ddc44057f5e6d76
Castro <igordc38@gmail.com>
2023-06-13 22:00:09 -0300

    ICA [environment] variables configuration on ts archive

---
+++ environments/dev.ts
@@ -0,0 +1,5 @@
+export const devEnvironment = {
+  production: false,
+  baseUrl: "https://localhost:7239/api",
+  encryptSecretKey: "b-!&<]TWpoAs6%ZZphX>53T8DFHzRo",
+};
---
+++ environments/prod.ts
@@ -0,0 +1,5 @@
+export const prodEnvironment = {
+  production: true,
+  baseUrl: "production Url Here",
+  encryptSecretKey: "secret here",
+};
